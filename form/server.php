1b79f0b7b4d2abca5de506044a5eea49daee4d33
NightingaleAcademy <99267602+NightingaleAcademy@users.noreply.github.com>
2022-02-26 07:41:37 +0545

    Update server.php

--- form/server.php
+++ form/server.php
@@ -1,9 +1,9 @@
 <?php 
 
-$host="localhost";
-$user="root";
-$pass="";
-$dbname="registration";
+$host="sql113.epizy.com";
+$user="	epiz_31091999";
+$pass="bkKm0fhnFHatb";
+$dbname="epiz_31091999_regform";
 
 $conn=mysqli_connect($host,$user,$pass,$dbname) or die();
 
